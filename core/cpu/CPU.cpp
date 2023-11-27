276ce600c804bd60cab27540b522157c1eb9c39f
asaichi8 <>
2023-11-27 06:55:12 +0000

    made reset function more accurate

--- core/cpu/CPU.cpp
+++ core/cpu/CPU.cpp
@@ -24,12 +24,22 @@ void CPU::Execute(const Instruction& instruction)
 /// @brief Resets the processor to a known state.
 void CPU::Reset()
 {
-    reg.program_counter = 0;
+    // https://en.wikipedia.org/wiki/Reset_vector "The reset vector for 6502 processor family is a 16-bit address stored at 0xFFFC and 0xFFFD."
+    reg.program_counter = m_RAM->ReadWord(0xFFFC); 
     reg.accumulator = 0;
     reg.X = 0;
     reg.Y = 0;
     reg.stack_pointer = 0xFD; // https://www.nesdev.org/wiki/CPU_power_up_state#cite_note-reset-stack-push-3
+
     reg.status_register = 0;
+    reg.status_register.set(StatusRegisterFlags::UNUSED); // https://www.nesdev.org/wiki/Status_flags "No CPU effect; always pushed as 1"
+    // https://www.nesdev.org/wiki/CPU_power_up_state#cite_note-1
+    reg.status_register.set(StatusRegisterFlags::BREAK_COMMAND);
+    reg.status_register.set(StatusRegisterFlags::UNUSED);
+
+    // https://6502.co.uk/lesson/reset
+    // "This reset sequence lasts for seven clock cycles and after this, the computer will be usable. "
+    // TODO: cycles += 7;
 
     m_RAM->Reset();
 }
