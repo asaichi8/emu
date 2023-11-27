d77716fb238a359dfc2ad2f646d4a03ab152af57
asaichi8 <>
2023-11-27 09:15:25 +0000

    examples

--- app/main.cpp
+++ app/main.cpp
@@ -3,6 +3,7 @@
 #include "../core/cpu/CPU.h"
 
 #define KB 1024
+#define EG1 1
 
 int main()
 {
@@ -13,16 +14,17 @@ int main()
     
     // example code
     
+#ifdef EG1
     ram.WriteByte(0x1000, 0xA2); // LDX
     ram.WriteByte(0x1001, 0x0A); // #$0A
     ram.WriteByte(0x1002, 0xE8); // INX
     ram.WriteByte(0x1003, 0x8E); // STX
     ram.WriteWord(0x1004, 0x2000); // $2000
     ram.WriteByte(0x1006, 0x02); // KIL (illegal opcode - halts CPU)
+#endif
 
-    std::cout << (int)(ram.ReadByte(0x2000)) << std::endl;
-
-    /*{
+#ifndef EG1
+    {
         const char* sz = "Hello, World!";
         for (int i = 0; i < strlen(sz); ++i)
         {
@@ -34,13 +36,20 @@ int main()
             if (i == strlen(sz) - 1) // if it's the last iteration...
                 ram.WriteByte(0x1006 + (i*6), 0x02); // KIL (illegal opcode - halts CPU)
         }
-    }*/
+    }
+#endif
     
 
     cpu.Run();
 
-    /*for (int i = 0; i < 13; ++i)
-        std::cout << (char)(ram.ReadByte(0x2000 + i));*/
-        
+#ifdef EG1
+    std::cout << (int)(ram.ReadByte(0x2000)) << std::endl;
+#endif
+
+#ifndef EG1
+    for (int i = 0; i < 13; ++i)
+        std::cout << (char)(ram.ReadByte(0x2000 + i));
+#endif
+
     return 0;
 }
