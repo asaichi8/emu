9201e4beb9d05f2d4c293c0b17e98da6dc64f379
asaichi8 <>
2024-01-26 14:04:49 +0000

    add _MAX defs

--- include/typedefs.h
+++ include/typedefs.h
@@ -7,6 +7,11 @@
 #define DWORD   uint32_t
 #define QWORD   uint64_t
 
+#define BYTE_MAX UINT8_MAX
+#define WORD_MAX UINT16_MAX
+#define DWORD_MAX UINT32_MAX
+#define QWORD_MAX UINT64_MAX
+
 #define PAGE    0x0100
 
 #define STACK   0x0100 // beginning of stack (0x0100 - 0x01FF)
