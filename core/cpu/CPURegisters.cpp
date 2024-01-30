d55cb5cb8eaddfe99cd7f59643f67b855fe17ac1
asaichi8 <>
2024-01-30 11:46:32 +0000

    cleaner & clearer cpu register functions

--- core/cpu/CPURegisters.cpp
+++ core/cpu/CPURegisters.cpp
@@ -1,37 +1,27 @@
 #include "CPURegisters.h"
 
-// Checks if the value is zero, and sets register based on result
+/// @brief Checks if the value is zero, and sets register based on result
 void CPURegisters::CheckZero(BYTE val)
 {
-    if (val == 0x00)
-        this->status_register.set(StatusRegisterFlags::ZERO);
-    else
-        this->status_register.reset(StatusRegisterFlags::ZERO);
+    this->status_register.set(StatusRegisterFlags::ZERO, val == 0x00);
 }
 
-// Checks if the value is negative, and sets register based on result
+/// @brief Checks if the value is negative, and sets register based on result
 void CPURegisters::CheckNegative(BYTE val)
 {
-    if ((CHAR)val < 0)
-        this->status_register.set(StatusRegisterFlags::NEGATIVE);
-    else
-        this->status_register.reset(StatusRegisterFlags::NEGATIVE);
+    this->status_register.set(StatusRegisterFlags::NEGATIVE, (CHAR)val < 0);
 }
 
-// Checks if the value is has a carry, and sets register based on result
+/// @brief Checks if the value is has a carry, and sets register based on result
 void CPURegisters::CheckCarry(WORD val)
 {
     // set carry flag if we don't need to borrow, reset otherwise
-    if (val > BYTE_MAX)
-        this->status_register.set(StatusRegisterFlags::CARRY);
-    else
-        this->status_register.reset(StatusRegisterFlags::CARRY);
+    this->status_register.set(StatusRegisterFlags::CARRY, val > BYTE_MAX);
 }
 
+/// @brief In the comparison operations, the carry bit is set if the register value is greater
+///        than the read value.
 void CPURegisters::CheckCarryCompare(BYTE regVal, BYTE val) 
 {
-    if (regVal >= val)
-        this->status_register.set(StatusRegisterFlags::CARRY);
-    else
-        this->status_register.reset(StatusRegisterFlags::CARRY);
+    this->status_register.set(StatusRegisterFlags::CARRY, regVal >= val);
 }
