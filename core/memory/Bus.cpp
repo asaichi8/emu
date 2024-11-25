f6fc1b1ae0c575822d6251d068b2a8c349d3f52b
asaichi8 <>
2024-11-25 00:45:47 +0000

    added clarifying comments to reading on the bus

--- core/memory/Bus.cpp
+++ core/memory/Bus.cpp
@@ -14,10 +14,10 @@ void Bus::Reset()
 
 BYTE Bus::ReadPRGByte(WORD addr)
 {
-	addr -= 32 * KB;
+	addr -= 32 * KB; // normalise address - CPU addresses it from 32KiB to 64KiB
 	
 	if (m_ROM->PRG_ROM.size() == (16 * KB) && addr >= (16 * KB))
-		addr %= 16 * KB;
+		addr %= 16 * KB; // mirror address if it's outside the PRG ROM size range
 	
 	return m_ROM->PRG_ROM[addr];
 }
@@ -36,8 +36,11 @@ WORD Bus::ReadPRGWord(WORD addr, bool shouldWrapPage)
 
 BYTE Bus::ReadByte(WORD addr)
 {
+	// On the NES, only 2KiB of RAM is accessible due to hardware limitations. The CPU accesses RAM through addresses 0x0000 to 0x1FFF, however, which
+	// is an 8KiB range. As a result, any time the CPU attempts to access an address past 2KiB (0x800), the address is mirrored back to an address
+	// within the initial 2KiB.
 	if (addr < MIRRORED_INTERNAL_RAM_END)
-		return m_RAM[addr % INTERNAL_RAM_SIZE];
+		return m_RAM[addr % INTERNAL_RAM_SIZE]; // return mirrored address
 	else if (addr >= PRG_RAM_START && addr <= PRG_RAM_END)
 		return ReadPRGByte(addr);
 	else if (addr < MIRRORED_PPU_REGISTER_END)
