9f46f31b7e3bb84d9928e9822f4930caca38c99c
asaichi8 <>
2024-11-27 01:41:06 +0000

    complete PPUSTATUS register

--- core/ppu/registers/PPUSTATUS.h
+++ core/ppu/registers/PPUSTATUS.h
@@ -1,11 +1,27 @@
 #pragma once
 
+#include <bitset>
 #include "IPPURegister.h"
 
 
 class PPUSTATUS : public IPPURegister
 {
+    // https://www.nesdev.org/wiki/PPU_registers#PPUSTATUS_-_Rendering_events_($2002_read)
+    enum PPUStatusRegisterFlags
+    {
+        PPUIDENTIFIER0, // PPU identifier seems to be used for copy protection
+        PPUIDENTIFIER1,
+        PPUIDENTIFIER2,
+        PPUIDENTIFIER3,
+        PPUIDENTIFIER4,
+        SPRITE_OVERFLOW,
+        SPRITE_0_HIT,
+        VBLANK
+    };
+
 public:
     PPUSTATUS() : IPPURegister((WORD)PPURegAddr::PPUSTATUS) {}
     ~PPUSTATUS() override {}
+
+    std::bitset<8> ppu_status_register; // TODO: can/should this be abstracted further?
 };
