5ffdfce37c64fbe8e46d4989024d2ba12ca934be
asaichi8 <>
2024-11-29 15:14:06 +0000

    Write & GetAddress functions to PPUADDR.h

--- core/ppu/registers/PPUADDR.h
+++ core/ppu/registers/PPUADDR.h
@@ -3,9 +3,24 @@
 #include "IPPURegister.h"
 
 
+// https://www.nesdev.org/wiki/PPU_registers#PPUADDR_-_VRAM_address_($2006_write)
 class PPUADDR : public IPPURegister
 {
+    WORD ppuAddr = 0;
+
 public:
     PPUADDR(InternalRegisters* _internal_registers) : IPPURegister((WORD)PPURegAddr::PPUADDR, _internal_registers) {}
     ~PPUADDR() override {}
+
+    void Write(std::bitset<8> val) override 
+    { 
+        if (internal_registers->w) // write low byte
+            ppuAddr = (ppuAddr & 0xFF00) | (WORD)val.to_ulong();
+        else // write high byte
+            ppuAddr = (ppuAddr & 0x00FF) | ((WORD)val.to_ulong() << 8);
+
+        internal_registers->w = !internal_registers->w;
+    }
+
+    WORD GetAddress() { return ppuAddr; }
 };
