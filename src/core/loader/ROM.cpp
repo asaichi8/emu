d47d39aade8aee37be102be82c773dcd4b33cdb9
asaichi8 <>
2024-12-10 05:51:54 +0000

    use pointers to avoid calling LoadFile multiple times

--- src/core/loader/ROM.cpp
+++ src/core/loader/ROM.cpp
@@ -12,31 +12,34 @@ ROM::ROM()
 /// @brief Checks if the file loaded is a valid .nes file.
 /// @param filePath 
 /// @return The error message. Succeeded if nothing was returned.
-std::string ROM::CheckROM(const std::string& filePath)
+std::string ROM::CheckROM(const std::string& filePath, bool shouldUse)
 {
-	// load file
-	m_rawFile = Loader::LoadFile(filePath);
+	// avoid creating two vectors of bytes by using a pointer
+	std::unique_ptr<std::vector<BYTE>> pTempFile = std::make_unique<std::vector<BYTE>>(Loader::LoadFile(filePath));
+	std::vector<BYTE>* pFile = pTempFile.get();
+
+	if (shouldUse)
+		m_pRawFile = std::move(pTempFile);
 
 	// check we actually loaded something
-	if (m_rawFile.empty())
+	if (pFile->empty())
 		return "Cannot load an empty file!";
 
 	// check size
-	if (!CheckHeaderFits(m_rawFile))
+	if (!CheckHeaderFits(*pFile))
 		return "File not large enough to read header!";
 
-	{
-		// map first 16 bytes to iNES_Header
-		const iNES_Header* header = (const iNES_Header*)(m_rawFile.data());
+	// map first 16 bytes to iNES_Header
+	const iNES_Header* header = (const iNES_Header*)(pFile->data());
+	if (shouldUse)
 		m_inesHeader = *header; // create a copy of the header to store as a member variable
-	}
 	
 	// check that the file is an iNES file and file actually has space for data requested by header
-	if (!CheckHeaderIsINES(&m_inesHeader))
+	if (!CheckHeaderIsINES(header))
 		return "Not an NES file!";
-	if (!CheckFileSize(m_rawFile, &m_inesHeader))
+	if (!CheckFileSize(*pFile, header))
 		return "File not large enough to map data requested by header!";
-	if (GetINESVersion(&m_inesHeader) > 0) 
+	if (GetINESVersion(header) > 0) 
 		return "Only iNES 1.0 is supported at this time!";
 
 	return {};
@@ -48,7 +51,7 @@ std::string ROM::CheckROM(const std::string& filePath)
 std::string ROM::LoadROM(const std::string& filePath)
 {
 	// CheckROM fills out m_rawFile & m_inesHeader
-	std::string errMsg = CheckROM(filePath);
+	std::string errMsg = CheckROM(filePath, true);
 	if (!errMsg.empty())
 		return errMsg;
 
@@ -58,10 +61,10 @@ std::string ROM::LoadROM(const std::string& filePath)
 	size_t PRG_ROM_start = sizeof(iNES_Header) + ((m_inesHeader.flags6 & Flags6::HasTrainer) ? TRAINER_SIZE : 0);
 	size_t CHR_ROM_start = PRG_ROM_start + (m_inesHeader.PRG_ROM_banks * PRG_ROM_BANK_SIZE);
 
-	auto PRG_begin = m_rawFile.begin() + PRG_ROM_start;
-	auto PRG_end   = m_rawFile.begin() + PRG_ROM_start + (m_inesHeader.PRG_ROM_banks * PRG_ROM_BANK_SIZE);
-	auto CHR_begin = m_rawFile.begin() + CHR_ROM_start;
-	auto CHR_end   = m_rawFile.begin() + CHR_ROM_start + (m_inesHeader.CHR_ROM_banks * CHR_ROM_BANK_SIZE);
+	auto PRG_begin = m_pRawFile->begin() + PRG_ROM_start;
+	auto PRG_end   = m_pRawFile->begin() + PRG_ROM_start + (m_inesHeader.PRG_ROM_banks * PRG_ROM_BANK_SIZE);
+	auto CHR_begin = m_pRawFile->begin() + CHR_ROM_start;
+	auto CHR_end   = m_pRawFile->begin() + CHR_ROM_start + (m_inesHeader.CHR_ROM_banks * CHR_ROM_BANK_SIZE);
 
 	PRG_ROM.assign(PRG_begin, PRG_end);
 	CHR_ROM.assign(CHR_begin, CHR_end); 
--- src/core/loader/ROM.h
+++ src/core/loader/ROM.h
@@ -65,7 +65,7 @@ class ROM
 	const size_t PRG_ROM_BANK_SIZE = 16 * KB;
 	const size_t CHR_ROM_BANK_SIZE = 8 * KB;
 
-	std::vector<BYTE> m_rawFile{};
+	std::unique_ptr<std::vector<BYTE>> m_pRawFile{};
 	iNES_Header m_inesHeader{};
 	size_t m_nPRG_ROM_size{};
 	size_t m_nCHR_ROM_size{};
@@ -85,6 +85,6 @@ public:
 	BYTE mapperType{};
 	MirrorType mirrorType{};
 	
-	std::string CheckROM(const std::string& filePath);
+	std::string CheckROM(const std::string& filePath, bool shouldUse = false);
 	std::string LoadROM(const std::string& filePath);
 };
