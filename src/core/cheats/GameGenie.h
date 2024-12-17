775450aeb7b3ece41b51c54b52ee9894982eb444
asaichi8 <>
2024-12-17 20:52:45 +0000

    make game genie decode functions static

--- src/core/cheats/GameGenie.h
+++ src/core/cheats/GameGenie.h
@@ -11,6 +11,7 @@
 // https://github.com/jdratlif/ggencoder-java/blob/master/docs/nes.txt
 class GameGenie
 {
+    // string:   SXIOPO
     // from link above:
     // Code:     110110100101100100011001
     // Position: 1678H234-IJKLABCDMNO5EFG
@@ -28,12 +29,13 @@ class GameGenie
         0, 5, 6, 7, 16, 1, 2, 3, -1, 17, 18, 19, 20, 9, 10, 11, 12,
         21, 22, 23, 28, 13, 14, 15, 24, 29, 30, 31, 4, 25, 26, 27
     };
-    std::unordered_map<char, BYTE> HexTable = {
+    // in the game genie, each letter evaluates to an appropriate binary value
+    static const inline std::unordered_map<char, BYTE> HexTable = {
         {'A', 0x0}, {'P', 0x1}, {'Z', 0x2}, {'L', 0x3}, {'G', 0x4}, {'I', 0x5}, {'T', 0x6}, {'Y', 0x7},
         {'E', 0x8}, {'O', 0x9}, {'X', 0xA}, {'U', 0xB}, {'K', 0xC}, {'S', 0xD}, {'V', 0xE}, {'N', 0xF}
     };
 
-    std::vector<bool> StringCodeToBits(const std::string& code);
+    static std::vector<bool> StringCodeToBits(const std::string& code);
 
 public:
     GameGenie();
@@ -45,5 +47,5 @@ public:
         std::optional<BYTE> compare = std::nullopt;
     };
 
-    DecodedCode Decode(const std::string& code);
+    static DecodedCode Decode(const std::string& code);
 };
