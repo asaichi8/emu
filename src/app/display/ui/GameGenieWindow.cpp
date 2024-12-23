39798192adafa047e385e127d69ad1c3fe847eac
a <a@a.com>
2024-12-23 01:32:14 +0000

    add message if no codes & add window constraints

--- src/app/display/ui/GameGenieWindow.cpp
+++ src/app/display/ui/GameGenieWindow.cpp
@@ -16,36 +16,41 @@ void GameGenieWindow::Draw()
         
     // TODO: resize this window based on the length, and set description to right hand side
     // TODO: add "add code"
-    ImGui::SetNextWindowSize(ImVec2(400, 400), ImGuiCond_Always);
-    ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
-
-    ImGui::Begin("Game Genie Codes", &this->m_isOpen, ImGuiWindowFlags_AlwaysAutoResize);
+    ImGui::SetNextWindowSizeConstraints({0.f, 0.f}, {750.f, 500.f});
+    ImGui::Begin("Game Genie Codes", &this->m_isOpen, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_HorizontalScrollbar);
     
-    for (int i = 0; i < pGameInfo->gameGenieCodes.size(); ++i) // loop through each code
+    if (pGameInfo->gameGenieCodes.empty())
     {
-        GameGenie::GameGenieCode* code = &pGameInfo->gameGenieCodes.at(i);
-
-        ImGui::PushID(i); // give unique ID to each checkbox
-
-        if (ImGui::Checkbox("##EnableCode", &code->isActive))
-            pGameInfo->BuildCodeMap();
-
-        ImGui::SameLine();
-        ImGui::Text("%s :  %s", code->code.c_str(), code->description.c_str());
-        if (ImGui::IsItemHovered())
+        ImGui::Text("No codes are currently loaded.");
+    }
+    else
+    {
+        for (int i = 0; i < pGameInfo->gameGenieCodes.size(); ++i) // loop through each code
         {
-            ImGui::BeginTooltip();
-            ImGui::Text("Address = 0x%x", code->decoded.addr);
-            ImGui::Text("Value = 0x%x", code->decoded.val);
-            if (code->decoded.compare.has_value())
-                ImGui::Text("Compare = 0x%x", code->decoded.compare.value());
-            ImGui::EndTooltip();
-        }
-        //ImGui::SameLine();
-        //ImGui::SetCursorPosX(ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize(code->description.c_str()).x); // set to right hand side
-        //ImGui::Text("%s", code->description.c_str());
+            GameGenie::GameGenieCode* code = &pGameInfo->gameGenieCodes.at(i);
 
-        ImGui::PopID();
+            ImGui::PushID(i); // give unique ID to each checkbox
+
+            if (ImGui::Checkbox("##EnableCode", &code->isActive))
+                pGameInfo->BuildCodeMap();
+
+            ImGui::SameLine();
+            ImGui::Text("%s :  %s", code->code.c_str(), code->description.c_str());
+            if (ImGui::IsItemHovered())
+            {
+                ImGui::BeginTooltip();
+                ImGui::Text("Address = 0x%x", code->decoded.addr);
+                ImGui::Text("Value = 0x%x", code->decoded.val);
+                if (code->decoded.compare.has_value())
+                    ImGui::Text("Compare = 0x%x", code->decoded.compare.value());
+                ImGui::EndTooltip();
+            }
+            //ImGui::SameLine();
+            //ImGui::SetCursorPosX(ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize(code->description.c_str()).x); // set to right hand side
+            //ImGui::Text("%s", code->description.c_str());
+
+            ImGui::PopID();
+        }
     }
 
     ImGui::End();
