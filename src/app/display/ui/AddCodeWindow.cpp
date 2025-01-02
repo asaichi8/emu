082fb368822caf87e58261668a7348e22531e447
asaichi8 <>
2025-01-02 04:10:35 +0000

    create addr, val, compare entries

--- src/app/display/ui/AddCodeWindow.cpp
+++ src/app/display/ui/AddCodeWindow.cpp
@@ -12,7 +12,20 @@ void AddCodeWindow::Draw()
     ImGui::SetNextWindowSizeConstraints({0.f, 0.f}, {750.f, 500.f});
     ImGui::Begin("Add Game Genie Code", &this->m_isOpen, ImGuiWindowFlags_AlwaysAutoResize);
     
-
+    // IM_ARRAYSIZE - " Don't use on pointers!", so pass it here instead
+    CreateEntry("Address:", m_szAddress, IM_ARRAYSIZE(m_szAddress));
+    CreateEntry("Value:  ", m_szValue, IM_ARRAYSIZE(m_szValue));
+    CreateEntry("Compare:", m_szCompare, IM_ARRAYSIZE(m_szCompare));
 
     ImGui::End();
+}
+
+
+void AddCodeWindow::CreateEntry(const std::string& entryName, char* buffer, size_t bufSize)
+{
+    ImGui::Text("%s 0x", entryName.c_str());
+    ImGui::SameLine();
+
+    std::string textboxName = "##" + entryName;
+    ImGui::InputText(textboxName.c_str(), buffer, bufSize, ImGuiInputTextFlags_CharsHexadecimal);
 }
--- src/app/display/ui/AddCodeWindow.h
+++ src/app/display/ui/AddCodeWindow.h
@@ -8,9 +8,13 @@
 
 class AddCodeWindow : public IGUIWindow
 {
+    char m_szAddress[5]{};
+    char m_szValue[3]{};
+    char m_szCompare[3]{};
 
 public:
     AddCodeWindow(const std::string& name);
 
+    void CreateEntry(const std::string& entryName, char* buffer, size_t bufSize);
     void Draw() override;
 };
