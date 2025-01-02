6e185931fe6baffff0b064250f38070339ac8d7a
asaichi8 <>
2025-01-02 04:26:20 +0000

    add Code entry & save button

--- src/app/display/ui/AddCodeWindow.cpp
+++ src/app/display/ui/AddCodeWindow.cpp
@@ -12,20 +12,33 @@ void AddCodeWindow::Draw()
     ImGui::SetNextWindowSizeConstraints({0.f, 0.f}, {750.f, 500.f});
     ImGui::Begin("Add Game Genie Code", &this->m_isOpen, ImGuiWindowFlags_AlwaysAutoResize);
     
-    // IM_ARRAYSIZE - " Don't use on pointers!", so pass it here instead
-    CreateEntry("Address:", m_szAddress, IM_ARRAYSIZE(m_szAddress));
-    CreateEntry("Value:  ", m_szValue, IM_ARRAYSIZE(m_szValue));
-    CreateEntry("Compare:", m_szCompare, IM_ARRAYSIZE(m_szCompare));
+    // IM_ARRAYSIZE - "Don't use on pointers!", so pass it here instead
+    CreateEntry("Address: 0x", m_szAddress, IM_ARRAYSIZE(m_szAddress), ImGuiInputTextFlags_CharsHexadecimal);
+    CreateEntry("Value:   0x", m_szValue,   IM_ARRAYSIZE(m_szValue)  , ImGuiInputTextFlags_CharsHexadecimal);
+    CreateEntry("Compare: 0x", m_szCompare, IM_ARRAYSIZE(m_szCompare), ImGuiInputTextFlags_CharsHexadecimal);
+
+    ImGui::Spacing();
+    ImGui::Separator();
+    ImGui::Spacing();
+
+    CreateEntry("Code:      ", m_szCode, IM_ARRAYSIZE(m_szCode));
+
+    ImGui::Spacing();
+
+    if (ImGui::Button("Add to code list", ImVec2(ImGui::GetContentRegionAvail().x, 0)))
+    {
+        // TODO: implement add to code list
+    }
 
     ImGui::End();
 }
 
 
-void AddCodeWindow::CreateEntry(const std::string& entryName, char* buffer, size_t bufSize)
+void AddCodeWindow::CreateEntry(const std::string& entryName, char* buffer, size_t bufSize, ImGuiInputTextFlags flags)
 {
-    ImGui::Text("%s 0x", entryName.c_str());
+    ImGui::Text("%s", entryName.c_str());
     ImGui::SameLine();
 
     std::string textboxName = "##" + entryName;
-    ImGui::InputText(textboxName.c_str(), buffer, bufSize, ImGuiInputTextFlags_CharsHexadecimal);
+    ImGui::InputText(textboxName.c_str(), buffer, bufSize, flags);
 }
--- src/app/display/ui/AddCodeWindow.h
+++ src/app/display/ui/AddCodeWindow.h
@@ -11,10 +11,11 @@ class AddCodeWindow : public IGUIWindow
     char m_szAddress[5]{};
     char m_szValue[3]{};
     char m_szCompare[3]{};
+    char m_szCode[9]{};
 
 public:
     AddCodeWindow(const std::string& name);
 
-    void CreateEntry(const std::string& entryName, char* buffer, size_t bufSize);
+    void CreateEntry(const std::string& entryName, char* buffer, size_t bufSize, ImGuiInputTextFlags flags = 0);
     void Draw() override;
 };
