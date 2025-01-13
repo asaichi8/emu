20c1d35cde6ea5a4941893e02976eaf99f5fd443
asaichi8 <>
2025-01-13 15:17:56 +0000

    clean up disassembler

--- src/core/cpu/Disassembler.cpp
+++ src/core/cpu/Disassembler.cpp
@@ -12,110 +12,129 @@ Disassembler::DisasmInfo Disassembler::Disassemble(WORD addr)
 {
     DisasmInfo info;
 
-	//WORD startAddr = addr;
-
 	// grab instruction
 	BYTE opcode = m_Bus->ReadByte(addr++);
 
 	// look up instruction in instruction table
 	const CPU::Instruction& instruction = CPU::GetInstructionTable()[opcode];
 	
-	// get name of instruction
 	info.instruction = instruction.strName;
 	info.addrMode = instruction.strAddrMode;
-    info.size = 1;
+    info.size = 1; // there's only one opcode, so we start the size off at 1
 
-    // set up stringstream used to convert vals to hex
-	std::stringstream ss;
-    ss << std::hex << std::setfill('0') << std::uppercase;
-
-    // https://www.nesdev.org/obelisk-6502-guide/addressing.html
-	if (info.addrMode == "IMP")
-	{
-		//
-	}
-	else if (info.addrMode == "ACC")
-	{
-		info.instruction += " A";
-	}
-	else if (info.addrMode == "IMM")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		ss << std::setw(2) << (int)val;
-		info.instruction += " #$" + ss.str();
-	}
-	else if (info.addrMode == "ABS")
-	{
-		auto val = m_Bus->ReadWord(addr);
-        info.size += 2;
-		ss << std::setw(4) << (int)val;
-		info.instruction += " $" + ss.str();
-	}
-	else if (info.addrMode == "ABX")
-	{
-		auto val = m_Bus->ReadWord(addr);
-        info.size += 2;
-		ss << std::setw(4) << (int)val;
-		info.instruction += " $" + ss.str() + ", X";
-	}
-	else if (info.addrMode == "ABY")
-	{
-		auto val = m_Bus->ReadWord(addr);
-        info.size += 2;
-		ss << std::setw(4) << (int)val;
-		info.instruction += " $" + ss.str() + ", Y";
-	}
-	else if (info.addrMode == "IND")
-	{
-		auto val = m_Bus->ReadWord(addr);
-        info.size += 2;
-		ss << std::setw(4) << (int)val;
-		info.instruction += " ($" + ss.str() + ")";
-	}
-	else if (info.addrMode == "REL")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		WORD branchAddr = addr + val; // TODO: check for overflow?
-		ss << std::setw(4) << (int)branchAddr;
-		info.instruction += " $" + ss.str();
-	}
-	else if (info.addrMode == "ZPG")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		ss << std::setw(2) << (int)val;
-		info.instruction += " $00" + ss.str();
-	}
-	else if (info.addrMode == "ZPX")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		ss << std::setw(2) << (int)val;
-		info.instruction += " $00" + ss.str() + ", X";
-	}
-	else if (info.addrMode == "ZPY")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		ss << std::setw(2) << (int)val;
-		info.instruction += " $00" + ss.str() + ", Y";
-	}
-	else if (info.addrMode == "IZY")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		ss << std::setw(2) << (int)val;
-		info.instruction += " ($" + ss.str() + "), Y";
-	}
-	else if (info.addrMode == "IZX")
-	{
-		auto val = m_Bus->ReadByte(addr);
-        info.size++;
-		ss << std::setw(2) << (int)val;
-		info.instruction += " ($" + ss.str() + ", X)";
-	}
+	// look up and call the addressing mode handler denoted by the addressing mode string
+	// these functions write to the DisasmInfo object given, appending the operand string to
+	// info->instruction and modifying info->size depending on the size of the operand
+	addrModeHandlers.at(info.addrMode)(&info, addr);
 
 	return info;
+}
+
+
+template <typename T, typename>
+std::string Disassembler::ValToHexStr(T val)
+{
+    std::stringstream ss;
+	ss << std::hex << std::setfill('0') << std::uppercase << std::setw(sizeof(T) * 2) << (int)val;
+
+	return ss.str();
+}
+
+void Disassembler::HandleIMP(DisasmInfo* info, WORD addr)
+{
+	//
+}
+
+void Disassembler::HandleACC(DisasmInfo* info, WORD addr)
+{
+	info->instruction += " A";
+}
+
+void Disassembler::HandleIMM(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+
+	info->instruction += " #$" + ValToHexStr(val);
+}
+
+void Disassembler::HandleABS(DisasmInfo* info, WORD addr)
+{
+	WORD val = m_Bus->ReadWord(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " $" + ValToHexStr(val);
+}
+
+void Disassembler::HandleABX(DisasmInfo* info, WORD addr)
+{
+	WORD val = m_Bus->ReadWord(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " $" + ValToHexStr(val) + ", X";
+}
+
+void Disassembler::HandleABY(DisasmInfo* info, WORD addr)
+{
+	WORD val = m_Bus->ReadWord(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " $" + ValToHexStr(val) + ", Y";
+}
+
+void Disassembler::HandleIND(DisasmInfo* info, WORD addr)
+{
+	WORD val = m_Bus->ReadWord(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " ($" + ValToHexStr(val) + ")";
+}
+
+void Disassembler::HandleREL(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+	WORD branchAddr = addr + val; // TODO: check for overflow?
+	
+    info->instruction += " $" + ValToHexStr(branchAddr);
+}
+
+void Disassembler::HandleZPG(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " $00" + ValToHexStr(val);
+}
+
+void Disassembler::HandleZPX(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " $00" + ValToHexStr(val) + ", X";
+}
+
+void Disassembler::HandleZPY(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " $00" + ValToHexStr(val) + ", Y";
+}
+
+void Disassembler::HandleIZY(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " ($" + ValToHexStr(val) + "), Y";
+}
+
+void Disassembler::HandleIZX(DisasmInfo* info, WORD addr)
+{
+	BYTE val = m_Bus->ReadByte(addr);
+	info->size += sizeof(val);
+    
+	info->instruction += " ($" + ValToHexStr(val) + ", X)";
 }
--- src/core/cpu/Disassembler.h
+++ src/core/cpu/Disassembler.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+#include <type_traits>
 #include "../memory/Bus.h"
 
 
@@ -7,17 +9,60 @@ class CPU;
 
 class Disassembler
 {
+public:
+    struct DisasmInfo
+    {
+        std::string instruction{}; // opcode + operand string
+        size_t size{};             // size of instruction
+        std::string addrMode{};    // string denoting addressing mode
+    };
+
+private:
 	std::shared_ptr<Bus> m_Bus{};
+
+    // SFINAE: only compile templated functions that are of integral type (e.g. BYTE, WORD etc.)
+    template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+    std::string ValToHexStr(T val);
+
+    // handler functions operate on a DisasmInfo pointer and take an addr that the bus will read from to get the operand
+#define INSTRUCTION_HANDLER(handlerName) void handlerName(DisasmInfo* info, WORD addr)
+    INSTRUCTION_HANDLER(HandleIMP);
+    INSTRUCTION_HANDLER(HandleACC);
+    INSTRUCTION_HANDLER(HandleIMM);
+    INSTRUCTION_HANDLER(HandleABS);
+    INSTRUCTION_HANDLER(HandleABX);
+    INSTRUCTION_HANDLER(HandleABY);
+    INSTRUCTION_HANDLER(HandleIND);
+    INSTRUCTION_HANDLER(HandleREL);
+    INSTRUCTION_HANDLER(HandleZPG);
+    INSTRUCTION_HANDLER(HandleZPX);
+    INSTRUCTION_HANDLER(HandleZPY);
+    INSTRUCTION_HANDLER(HandleIZY);
+    INSTRUCTION_HANDLER(HandleIZX);
+
+	// string denoting the addressing mode (e.g. "IMM" == &CPU::IMM), followed by a function pointer pointing to a
+	// lambda function that captures the current instance of this class and calls the accompanying addressing mode handler
+	// (e.g. "IMM" is accompanied by this->HandleIMM(info, addr); )
+#define HANDLE_PAIR(addrMode) {#addrMode, [this](DisasmInfo* info, WORD addr) { Handle##addrMode(info, addr); }}
+	const std::unordered_map<std::string, std::function<void(DisasmInfo*, WORD)>> addrModeHandlers =
+	{
+		HANDLE_PAIR(IMP),
+		HANDLE_PAIR(ACC),
+		HANDLE_PAIR(IMM),
+		HANDLE_PAIR(ABS),
+		HANDLE_PAIR(ABX),
+		HANDLE_PAIR(ABY),
+		HANDLE_PAIR(IND),
+		HANDLE_PAIR(REL),
+		HANDLE_PAIR(ZPG),
+		HANDLE_PAIR(ZPX),
+		HANDLE_PAIR(ZPY),
+		HANDLE_PAIR(IZY),
+		HANDLE_PAIR(IZX)
+	};
 
 public:
     Disassembler(std::shared_ptr<Bus> bus);
-
-    struct DisasmInfo
-    {
-        std::string instruction{};
-        size_t size{};
-        std::string addrMode{};
-    };
 
     DisasmInfo Disassemble(WORD addr);
 };
